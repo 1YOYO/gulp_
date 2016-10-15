d65ac89903fc2e198e64f1518c972405e31424d8
unknown <>
2016-10-15 10:14:58 +0800

    gulp目录结构

--- app/app.js
+++ app/app.js
@@ -1,15 +1,11 @@
-/*
-* @Author: 虚竹
-* @Date:   2016-09-29 09:29:36
-* @Last Modified by:   YOYO
-* @Last Modified time: 2016-10-15 09:33:49
-*/
 
 ;(function(angular) {
 	'use strict';
-	var app = angular.module("app", []);
+	var app = angular.module("app", ['ngRoute']);
 
 	app.controller('appController', ['$scope', function($scope){
-		
+		$scope.appdata={
+			name: "apppages"
+		}
 	}]);
 })(angular);
--- app/route.js
+++ app/route.js
@@ -1,19 +1,14 @@
-/*
-* @Author: 虚竹
-* @Date:   2016-09-29 09:29:36
-* @Last Modified by:   YOYO
-* @Last Modified time: 2016-10-15 09:34:46
-*/
+
 
 ;(function(angular) {
 	'use strict';
 	var app = angular.module("app");
 	app.config(['$routeProvider', function($routeProvider) {
-		$routeProvider.when('', {
-			templateUrl: '',
-			controller: ''
+		$routeProvider.when('/hot', {
+			templateUrl: '../views/hot/view.html',
+			controller: 'hotController'
 		}).otherwise({
-			redirectTo: ''
+			redirectTo: '/'
 		});
 	}]);
 })(angular);
